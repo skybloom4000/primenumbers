f1625ed42e02bf39778cae47a31cdf85ccc886cd
skybloom4000 <150357917+skybloom4000@users.noreply.github.com>
2024-07-18 15:46:48 +0300

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,34 @@
+public class PrimeNumber {
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num == 2 || num == 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+        int i = 5;
+        while (i * i <= num) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+            i += 6;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // Display prime numbers between 1 to 500
+        System.out.println("Prime numbers between 1 to 500:");
+        for (int i = 1; i <= 500; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
+
